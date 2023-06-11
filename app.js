7aa47a727826e316611125059e8473fb9ec0e622
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-06-11 20:18:18 +0530

    Programming for functionalities done in app.js

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var btnTranslate = document.querySelector("#clcik");
-var out = document.querySelector("#output");
+var txtar = document.querySelector("#txtin")
 
-btnTranslate.addEventListener("click",function clickEventhandler()
-{
-    console.log("again whyyy")
-    console.log(txtar.value)
-    out.innerText ="max ers"+txtar.value
+var out = document.querySelector("#output");
+var serverUrl = "	https://api.funtranslations.com/translate/minion.json";
+btnTranslate.addEventListener("click", function clickEventhandler() {
+    // console.log("again whyyy")
+     console.log(txtar.value)
+    // out.innerText ="max ers"+txtar.value
+    var txtAInput = txtar.value
+    fetch(getTranslation(txtAInput)).then(response => response.json()).then(json =>
+
+
+        {
+            var translatedText = json.contents.translated;
+            console.log(translatedText)
+            out.innerText=translatedText;
+        }).catch(errorHanler)
+
+
 
 
 })
 
-var txtar = document.querySelector("#txtin")
+function errorHanler(error) {
+    console.log("Error Occured", error);
+    alert("Something went wrong try again later")
+}
+
+function getTranslation(text) {
+    return serverUrl + "?" + "text=" + text
+}
