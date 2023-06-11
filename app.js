179cf7e3f0ce5d5666d343b6a83eac64b70ed46d
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-06-11 19:17:06 +0530

    working on js

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-var name = prompt("Give me money")
-alert("hello welcome "+name)
+var btnTranslate = document.querySelector("#clcik");
+var out = document.querySelector("#output");
+
+btnTranslate.addEventListener("click",function clickEventhandler()
+{
+    console.log("again whyyy")
+    console.log(txtar.value)
+    out.innerText ="max ers"+txtar.value
+
+
+})
+
+var txtar = document.querySelector("#txtin")
